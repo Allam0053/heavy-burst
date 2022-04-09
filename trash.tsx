050c64ce6e9f98bfde9c3214a5e2fbd1bfe337f5
Allam Taju Sarof <55141556+Allam0053@users.noreply.github.com>
2022-04-09 14:54:14 +0700

    delete trash.tsx

--- trash.tsx
+++
@@ -1,5 +0,0 @@
-// circle
-<div className='h-48 w-48 rounded-full bg-cyan2 opacity-10 absolute top-16 left-8'></div>
-
-
-bg-[url("/decoration/Vector.svg"),url("/decoration/Vector1.svg")] bg-pos [--bg-position:left_bottom,left_bottom] opacity-50 bg-no-repeat
